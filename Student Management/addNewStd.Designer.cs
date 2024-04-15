376d81b91c899a44a58951068369597be576744e
Phi Vo <voduchoangphi@gmail.com>
2024-04-15 16:16:37 +0700

    add instruction function, list all users, reset password

---
+++ Student Management/addNewStd.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Student_Management
+{
+    partial class addNewStd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fullnameInput = new System.Windows.Forms.TextBox();
+            this.dob = new System.Windows.Forms.DateTimePicker();
+            this.cccdInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.femaleRadio = new System.Windows.Forms.RadioButton();
+            this.maleRadio = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fullnameInput
+            // 
+            this.fullnameInput.Location = new System.Drawing.Point(182, 46);
+            this.fullnameInput.Name = "fullnameInput";
+            this.fullnameInput.Size = new System.Drawing.Size(100, 22);
+            this.fullnameInput.TabIndex = 0;
+            // 
+            // dob
+            // 
+            this.dob.Location = new System.Drawing.Point(155, 135);
+            this.dob.Name = "dob";
+            this.dob.Size = new System.Drawing.Size(200, 22);
+            this.dob.TabIndex = 1;
+            // 
+            // cccdInput
+            // 
+            this.cccdInput.Location = new System.Drawing.Point(155, 284);
+            this.cccdInput.Name = "cccdInput";
+            this.cccdInput.Size = new System.Drawing.Size(100, 22);
+            this.cccdInput.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.femaleRadio);
+            this.groupBox1.Controls.Add(this.maleRadio);
+            this.groupBox1.Location = new System.Drawing.Point(142, 207);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(299, 42);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // femaleRadio
+            // 
+            this.femaleRadio.AutoSize = true;
+            this.femaleRadio.Location = new System.Drawing.Point(191, 7);
+            this.femaleRadio.Name = "femaleRadio";
+            this.femaleRadio.Size = new System.Drawing.Size(74, 20);
+            this.femaleRadio.TabIndex = 1;
+            this.femaleRadio.TabStop = true;
+            this.femaleRadio.Text = "Female";
+            this.femaleRadio.UseVisualStyleBackColor = true;
+            // 
+            // maleRadio
+            // 
+            this.maleRadio.AutoSize = true;
+            this.maleRadio.Location = new System.Drawing.Point(26, 5);
+            this.maleRadio.Name = "maleRadio";
+            this.maleRadio.Size = new System.Drawing.Size(58, 20);
+            this.maleRadio.TabIndex = 0;
+            this.maleRadio.TabStop = true;
+            this.maleRadio.Text = "Male";
+            this.maleRadio.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(348, 342);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // addNewStd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cccdInput);
+            this.Controls.Add(this.dob);
+            this.Controls.Add(this.fullnameInput);
+            this.Name = "addNewStd";
+            this.Text = "addNewStd";
+            this.Load += new System.EventHandler(this.addNewStd_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fullnameInput;
+        private System.Windows.Forms.DateTimePicker dob;
+        private System.Windows.Forms.TextBox cccdInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton femaleRadio;
+        private System.Windows.Forms.RadioButton maleRadio;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Student Management/resetPassword.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_Management
+{
+    public partial class resetPassword : Form
+    {
+        public resetPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string newPassword = passwordInput.Text.Trim() == "" ? "a" : passwordInput.Text;
+            SqlCommand cmd = new SqlCommand($"UPDATE ACCOUNT SET password = '{newPassword}' WHERE username='{usernameInput.Text}'",GlobalVars.connection);
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                label1.Text = ex.Message;
+            }
+        }
+    }
+}
